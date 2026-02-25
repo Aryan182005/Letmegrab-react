709cbff01f1bd4bb66bc9a6ea6fcdb1805988c7d
unknown <aryanchodvadiya0990@gmail.com>
2026-02-25 22:53:18 +0530

    first commit

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,18 @@
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex h-screen bg-[#0F0F0F]">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 overflow-auto">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
